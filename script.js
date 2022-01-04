39666f24b3b494695c76de4f6db58f9f0513789b
sam-cowing <sam.ryan.cowing@gmail.com>
2022-01-04 12:24:54 -0500

    Black pieces can now move one diagonal forward

--- script.js
+++
@@ -1,7 +0,0 @@
-// Pseudocoding for JS
-
-// Needs: 
-// - make a representation of the board on the back-end (use an array object)
-// - make DOM variables
-// - create turns and scores
-// 
--- scripts.js
+++ scripts.js
@@ -30,25 +30,25 @@ const board = [
     21, null, 22, null, 23, null, 24, null
 ]
 // 1
-start.addEventListener("click", function() {
+/*start.addEventListener("click", function() {
     console.log("1")
     bTurn()
-})
+})*/
 // 2
-function bTurn() {
+//function bTurn() {
     for (let i = 0; i < 12; i++) {
         bPieces[i].addEventListener("click", clickHandler)
         //bPieces[i].draggable = true
     }
-    console.log("2")
-} 
+    //console.log("2")
+//} 
 // 3
 function rTurn() {
     for (let i = 0; i < 12; i++) {
         rPieces[i].addEventListener("click", clickHandler3)
         //bPieces[i].draggable = true
     }
-    console.log("3")
+    //console.log("3")
 } 
 // 4
 function clearActive(pieces) {
@@ -56,7 +56,7 @@ function clearActive(pieces) {
         pieces[i].style.boxShadow = null
         //console.log(squares[i].style)
     }
-    console.log("4")
+    //console.log("4")
 } 
 //console.log(checkActive(bPieces))
 // 5
@@ -66,7 +66,7 @@ function clickHandler(evt) {
         bSelected.style.boxShadow = null 
         //console.log(bSelected.offsetParent.cellIndex)
         //console.log(bSelected.offsetParent.parentElement.rowIndex)
-        console.log("5a")
+        //console.log("5a")
     }
     else {
         clearActive(bPieces)
@@ -75,7 +75,7 @@ function clickHandler(evt) {
         //bRow = bSelected.parentElement.parentElement.rowIndex
         //bColumn = 4
         //bRow = 1
-        console.log("5b")
+        //console.log("5b")
     }
     //console.log(bColumn)
     //console.log(bRow)
@@ -85,32 +85,51 @@ function clickHandler(evt) {
     //availableSpots(evt)
     child = bSelected
     parent = bSelected.parentElement
-    console.log("5")
-    availableSpots()
+    spotChoices()
+    //console.log("5")
+    //availableSpots()
 }   
 // 6
 function clickHandler3(evt) {
     rSelected = evt.target
     if (rSelected.style.boxShadow === "rgb(60, 190, 199) 0px 0px 10px 5px") {
         rSelected.style.boxShadow = null 
-        console.log("6a")
+        //console.log("6a")
     }
     else {
         clearActive(bPieces)
         rSelected.style.boxShadow = "0 0 10px 5px rgb(60, 190, 199)"
-        console.log("6b")
+        //console.log("6b")
     }
     //console.log(evt)
     //availableSpots(evt)
     child = rSelected
     parent = rSelected.parentElement
-    console.log("6")
+    //console.log("6")
 }
+// new
+function spotChoices() {
+    let spot1 = []
+    let spot2 = []
+    console.log(parent)
+    y = parent.cellIndex
+    x = parent.parentElement.rowIndex
+    y1 = y - 1
+    newX = x - 1
+    y2 = y + 1
+    //console.log([newX, y1], [newX, y2])
+    spot1.push(newX, y1)
+    console.log(spot1)
+    spot2.push(newX, y2)
+    console.log(spot2)
+    availableSpots()
+}
+
 // 7
 function availableSpots() {
     for (let i = 0; i < 32; i++) {
-        if (squares[i].childElementCount === 0) {
-            console.log(true)
+        if (squares[i].childElementCount === 0 && squares[i].parentElement.rowIndex === newX && (squares[i].cellIndex === y1 || squares[i].cellIndex === y2)) {
+            //console.log(true)
             squares[i].style.border = "2px solid red"
             //console.log(squares[i].cellIndex)
             //console.log(squares[i].parentElement.rowIndex)
@@ -124,11 +143,11 @@ function availableSpots() {
                 console.log("7a")
             }) 
         } else {
-            console.log(false)
-            console.log("7b")
+            //console.log(false)
+            //console.log("7b")
         }
         rTurn()
-    } console.log("7")
+    } //console.log("7")
 }
 
 console.log("Done?")
